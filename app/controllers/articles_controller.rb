0a0849a26ff26e82a84006bf7415d82304a997f3
Damion Gibbs <dmgibbs@writeme.com>
2018-09-13 02:03:43 +0000

    Completed blog exercise. added authentication & comment deletion routines.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,7 @@
 class ArticlesController < ApplicationController
+	http_basic_authenticate_with name:  "dhh" , password:  "secret",
+	except:  [:index, :show]
+
 	def show
 		@article = Article.find(params[:id])
 	end
@@ -36,7 +39,12 @@ class ArticlesController < ApplicationController
 		end
 	end
 
-
+	def destroy
+		@article = Article.find(params[:id])
+		@article.destroy
+	 
+		redirect_to articles_path
+	end
 
 	private
 		def article_params
